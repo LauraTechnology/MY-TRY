95ac6181048bffb07f2e2522ab0619e5051b3093
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-09-14 12:54:45 +0300

    ORAI ,)

---
+++ if-else-if.js
@@ -0,0 +1,13 @@
+const oras = 'Debesuota';
+
+if (oras === 'Saulėta') {
+    console.log('Gera diena išeiti pasivaikščioti į mišką 🌞');
+} else if (oras === 'Lietinga') {
+    console.log('Pats laikas prisiminti kas yra šachmatai 🌰');
+} else if (oras === 'Vėjuota') {
+    console.log('Skrybelę geriau palikti namie, nes galimai jai gali užaugti sparnai');
+} else if (oras === 'Rūkas') {
+    console.log('Ar tavo automobilio rūko žibintai tikrai veikia?');
+} else if (oras === 'Debesuota') {
+    console.log('Šiandien lauke pilka ir niūru, pasidaryk skanios arbatos!');
+}
