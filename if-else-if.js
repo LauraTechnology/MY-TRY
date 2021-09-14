f9ce1e33b0364463f587199ff415987bd83eb0d2
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-09-14 15:01:41 +0300

    #2 užrašymo būdas

--- if-else-if.js
+++ if-else-if.js
@@ -5,11 +5,35 @@ if (oras === 'Saulėta') {
 } else if (oras === 'Lietinga') {
     console.log('Pats laikas prisiminti kas yra šachmatai 🌰');
 } else if (oras === 'Vėjuota') {
-    console.log('Skrybelę geriau palikti namie, nes galimai jai gali užaugti sparnai');
+    console.log('Skrybelę geriau palikti namie');
 } else if (oras === 'Rūkas') {
-    console.log('Ar tavo automobilio rūko žibintai tikrai veikia?');
+    console.log('Ar tavo automobilis turi rūko žibintus?');
 } else if (oras === 'Debesuota') {
-    console.log('Šiandien lauke pilka ir niūru, pasidaryk skanios arbatos!');
+    console.log('Šiandien lauke pilka ir niūru, geriau lik namie 🍵');
 } else {
     console.log('Oras neįtakoja mano nuotaikos 😉');
+}
+
+if (oras === 'Saulėta') {
+    console.log('Gera diena išeiti pasivaikščioti į mišką 🌞');
+} else {
+    if (oras ==== 'Lietinga') {
+        console.log('Pats laikas prisiminti kas yra šachmatai 🌰');
+    } else {
+        if () {
+            console.log();
+        } else {
+            if () {
+                console.log();
+            } else {
+                if () {
+                    console.log();
+                } else {
+                    if () {
+                        console.log();
+                    }
+                }
+            }
+        }
+    }
 }
